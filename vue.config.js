e9356904fcecc0cfd9bb6304f06246f0fa505367
trevor-r-allen <tallen2699@gmail.com>
2021-01-22 10:04:39 -0700

    added public path for github

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
- // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
-  publicPath: '/',
+  // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
+  publicPath: '/trevor-r-allen/',
   outputDir: 'docs',
   productionSourceMap: false
 }
